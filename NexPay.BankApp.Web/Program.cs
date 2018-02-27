dfb2d96077b35be6b922b6958645505a19fbb6fe
dremlin2000 <dremlin2000@gmail.com>
2018-02-27 14:33:26 +1100

    NLog setup added

--- NexPay.BankApp.Web/Program.cs
+++ NexPay.BankApp.Web/Program.cs
@@ -6,20 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
-namespace NexPay.BankApp.Web
+namespace NexPay.BankApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureAppConfiguration((builderContext, config) => {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    //logging.ClearProviders();
+                    //logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: setup NLog for Dependency injection
                 .Build();
     }
 }
